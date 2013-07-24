b263d6c8ca1ee9872ac9bf11d20d1c100d67d68b
Bora Yalcin <toraboranere@gmail.com>
2013-07-24 23:59:33 +0300

    header changes

--- header.php
+++ header.php
@@ -24,11 +24,9 @@
 
 	<!-- ÖRNEK HEADER: SİLİNEBİLİR-->
 	<header id="masthead" class="site-header" role="banner">
-		<div class="site-branding">
-			<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-			<h2 class="site-description"><?php bloginfo( 'description' ); ?></h2>
+		<div class="logo">
+			
 		</div>
-
 		<nav id="site-navigation" class="main-navigation" role="navigation">
 			
 			<?php wp_nav_menu( array( 'theme_location' => 'primary' ) ); ?>
@@ -53,10 +51,6 @@
 				console.log(d);
 				console.log('hello');
 			});
-
-			
-
-
 
 		});
 
