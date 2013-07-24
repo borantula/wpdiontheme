da8c15bef7657c431521ec043e5865eb09bc6cea
Bora Yalcin <toraboranere@gmail.com>
2013-07-25 00:11:12 +0300

    header changes deleted

--- header.php
+++ header.php
@@ -24,9 +24,11 @@
 
 	<!-- ÖRNEK HEADER: SİLİNEBİLİR-->
 	<header id="masthead" class="site-header" role="banner">
-		<div class="logo">
-			
+		<div class="site-branding">
+			<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+			<h2 class="site-description"><?php bloginfo( 'description' ); ?></h2>
 		</div>
+
 		<nav id="site-navigation" class="main-navigation" role="navigation">
 			
 			<?php wp_nav_menu( array( 'theme_location' => 'primary' ) ); ?>
@@ -34,35 +36,4 @@
 	</header><!-- #masthead -->
 
 	<div id="main" class="site-main">
-		<!-- / ÖRNEK HEADER: SİLİNEBİLİR-->
-
-	<button id="success">Yes</button>
-	<button id="fail">No</button>
-
-
-	<script type="text/javascript">
-
-	jQuery(document).ready(function($){
-		console.log('asdf');
-
-		$('#success').click(function(){
-
-			$.post(amadeusAjaxUrl('tester-event'),{success : 'yes'},function(d){
-				console.log(d);
-				console.log('hello');
-			});
-
-		});
-
-		$('#fail').click(function(){
-			$.post(amadeusAjaxUrl('tester-event'),{success:'no'},function(d){
-				console.log(d);
-			});
-		});
-
-	})
-
-
-	</script>
-
-	<?php 	echo get_option( 'dion-ajax-test'); ?>
+	<!-- / ÖRNEK HEADER: SİLİNEBİLİR-->
